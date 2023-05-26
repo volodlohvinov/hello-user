8749743c2d428dddab12f59aba906d16a575ae5a
Volodymyr Lohvinov <vladimirpravda2@gmail.com>
2023-05-26 15:57:10 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let name = prompt("What is your name?");
+
+if (name === null || name === "") {
+    alert("Hello, noname! How are you?");
+  }
+  else {
+    alert("Hello, " + name + "! How are you?")
+  }
+  
